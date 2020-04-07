c8df0be05bd95eaf35e63bb95c8b9d5bca7f2f00
Yohei Tamura <tamuhey@gmail.com>
2020-04-07 17:50:42 +0900

    modified:   setup.py to change pythonver

--- setup.py
+++ setup.py
@@ -37,11 +37,10 @@ setup(
         "Natural Language :: Japanese",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
 )
