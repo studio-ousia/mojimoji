523a01093a7517147bf405feb0c9c654c15a6ae0
Yohei Tamura <tamuhey@gmail.com>
2020-04-07 13:43:50 +0900

    modified setup.py to build package from pyx directly

--- setup.py
+++ setup.py
@@ -3,37 +3,45 @@
 import io
 import sys
 from setuptools import setup, Extension
+from Cython.Build import cythonize
 
-if sys.platform.startswith('win'):
+if sys.platform.startswith("win"):
     compile_args = []
     link_args = []
 else:
-    compile_args = ['-std=c++11']
-    link_args = ['-std=c++11']
+    compile_args = ["-std=c++11"]
+    link_args = ["-std=c++11"]
 
+extensions = [
+    Extension(
+        "mojimoji",
+        ["mojimoji.pyx"],
+        extra_compile_args=compile_args,
+        extra_link_args=link_args,
+    )
+]
 
 setup(
-    name='mojimoji',
-    version='0.0.9',
-    description='A fast converter between Japanese hankaku and zenkaku characters',
-    long_description=io.open('README.rst', encoding='utf-8').read(),
-    author='Studio Ousia',
-    author_email='ikuya@ousia.jp',
-    url='http://github.com/studio-ousia/mojimoji',
-    ext_modules=[Extension('mojimoji', ['mojimoji.cpp'], language='c++',
-                           extra_compile_args=compile_args, extra_link_args=link_args)],
-    keywords=['japanese'],
+    name="mojimoji",
+    version="0.0.9",
+    description="A fast converter between Japanese hankaku and zenkaku characters",
+    long_description=io.open("README.rst", encoding="utf-8").read(),
+    author="Studio Ousia",
+    author_email="ikuya@ousia.jp",
+    url="http://github.com/studio-ousia/mojimoji",
+    ext_modules=cythonize(extensions),
+    keywords=["japanese"],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Natural Language :: Japanese',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ]
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Natural Language :: Japanese",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
 )
