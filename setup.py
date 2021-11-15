188e26de9b6f9afce963969cef6af1f951384e7c
Ikuya Yamada <ikuya@ikuya.net>
2021-11-15 20:54:26 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ extensions = [
 
 setup(
     name="mojimoji",
-    version="0.0.11",
+    version="0.0.12",
     description="A fast converter between Japanese hankaku and zenkaku characters",
     long_description=io.open("README.rst", encoding="utf-8").read(),
     author="Studio Ousia",
